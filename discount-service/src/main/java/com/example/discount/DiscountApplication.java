f7e7cf1c01d2d2b2b5c64084c8b3f66d33cb460a
John James Libron <johnjames.libron@gmail.com>
2022-08-08 15:16:59 +0800

    Add discount to product list

--- discount-service/src/main/java/com/example/discount/DiscountApplication.java
+++ discount-service/src/main/java/com/example/discount/DiscountApplication.java
@@ -1,13 +1,31 @@
 package com.example.discount;
 
+import com.example.discount.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+import javax.annotation.processing.Processor;
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootApplication
+@EnableBinding(Processor.class)
 public class DiscountApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscountApplication.class, args);
+	}
+
+	public List<Product> addDiscountToProduct(List<Product> products) {
+		for (Product product : products) {
+			if (product.getPrice().compareTo(new BigDecimal(8000)) > 0) {
+
+			} else if (product.getPrice().compareTo(new BigDecimal(5000)) > 0) {
+
+			}
+		}
+		return null;
 	}
 
 }
---
+++ discount-service/src/main/java/com/example/discount/model/Product.java
@@ -0,0 +1,18 @@
+package com.example.discount.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
